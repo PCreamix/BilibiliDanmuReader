c4dda75739fc0479ef5cf8f37f21be5e469c2dde
Dreamix <pengchun1729@gmail.com>
2018-12-04 18:29:43 +0800

    打包和机器人对话功能==>_chat_with_bot

--- message_handle.py
+++ message_handle.py
@@ -3,27 +3,46 @@
 
 
 class MessageHandler:
-    def __init__(self, speaker, queue):
+    def __init__(self, speaker, queue, chatbot):
         self._speaker = speaker
         self._queue = queue
+        self._chatbot = chatbot
 
     async def read_loop(self):
         while True:
             message = await self._queue.get()
             msg_type = message[0]
-            if msg_type == 'comment':
+            if msg_type == 'DANMU_MSG':
                 # 弹幕消息
-                text = r"{} 说 {} o".format(*(message[1:]))
-            elif msg_type == 'gift':
+                uname, msg, userid = message[1:]
+                if uname == r'饼干喵7':
+                    uname = r'主播'
+                if msg.startswith(r'@' + self._chatbot.name):
+                    # 与聊天机器人对话
+                    await self._chat_with_bot(uname, msg, userid)
+                else:
+                    text = r"{} 说 {} o".format(uname, msg)
+                    await self._speaker.say(text)
+            elif msg_type == 'SEND_GIFT':
                 # 礼物消息
-                text = r"谢谢{}送的{}个{} o".format(*(message[1:]))
+                uname, num_gift, gift_type = message[1:]
+                text = r"谢谢{}送的{}个{} o".format(uname, num_gift, gift_type)
+                await self._speaker.say(text)
             else:
                 pass
-            await self._speaker.say(text)
+
+    async def _chat_with_bot(self, uname, msg, userid):
+        # 和聊天机器人对话
+        real_msg = msg.strip(r'@' + self._chatbot.name)  # 取出消息具体内容
+        reply = await self._chatbot.chat(userid, real_msg)  # 得到机器人的回复
+        question = r'{} 对 {} 说 {} o'.format(uname, self._chatbot.name, real_msg)
+        await self._speaker.say(question)
+        answer = r'{} 回答 {} 说 {} o'.format(self._chatbot.name, uname, reply)
+        await self._speaker.say(answer)
 
 
 def main():
-    print('ok')
+    print('no test')
 
 
 if __name__ == '__main__':
