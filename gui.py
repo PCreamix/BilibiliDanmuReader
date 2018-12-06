8465bff7e6518a17a7153af5ee98d0897be4e163
Dreamix <pengchun1729@gmail.com>
2018-12-06 18:08:58 +0800

    gui合并前准备 原理：开新线程注册循环任务，主线程不受干扰

--- gui.py
+++ gui.py
@@ -2,60 +2,104 @@
 # _*_ coding:utf-8 _*_
 
 from tkinter import *
+import asyncio
+from asyncio import Queue
+import threading
 
 
 class ApplicationGUI(Frame):
     def __init__(self, root=None):
         super().__init__(root)
         self.pack()
+        self.queue4log = Queue()
+        self.loop = None
+        self.roomid = IntVar()
+        self.roomid.set(6876276)
         self.createWidgets()
 
     def createWidgets(self):
-        operationPanel = self.operationPanel()
-        operationPanel.pack()
-        logInfoPanel = self.logInfoPanel()
-        logInfoPanel.pack()
+        self.create_operation_panel()
+        self.create_loginfo_panel()
 
-    def operationPanel(self):
-        root_frame = Frame(self)
-        notify_label = Label(root_frame, text=r'房间号：')
+    def create_operation_panel(self):
+        frm = Frame(self)
+        frm.pack()
+        notify_label = Label(frm, text=r'房间号：')
         notify_label.grid(row=0, column=0)
-        self.roomid = IntVar()
-        self.roomid.set(6876276)
-        entry = Entry(root_frame, textvariable=self.roomid)
+        entry = Entry(frm, textvariable=self.roomid)
         entry.grid(row=0, column=1)
-        button_frame = Frame(root_frame)
-        self.start = Button(button_frame, text=r'开始')
+        button_frame = Frame(frm)
+        self.start = Button(button_frame, text=r'开始', command=self.start)
         self.start.grid(row=0, column=0)
         self.stop = Button(button_frame, text=r'停止')
         self.stop.grid(row=0, column=1)
         button_frame.grid(row=0, column=2)
-        return root_frame
 
-    def logInfoPanel(self):
-        root_frame = Frame(self)
-        self.logger = Text(root_frame)
+    def create_loginfo_panel(self):
+        frm = Frame(self)
+        frm.pack()
+        self.logger = Text(frm)
         self.logger.pack(side=LEFT)
-        scrollbar = Scrollbar(root_frame, command=self.logger.yview)
+        scrollbar = Scrollbar(frm, command=self.logger.yview)
         self.logger['yscrollcommand'] = scrollbar.set
         scrollbar.pack(side=RIGHT, fill=Y)
-        return root_frame
 
-    def logout(self):
+    def logout(self, msg):
         self.logger.insert(END, '>> ')
-        self.logger.insert(END, self.roomid.get())
+        self.logger.insert(END, msg)
         self.logger.insert(END, '\n')
         self.logger.see(END)
 
     def start(self):
-        if self.client_loop:
+        # 停止旧协程和loop
+        if self.loop:
             self.stop()
-        roomid = self.roomid.get()
+        # 建立新协程
+        client_loop_coro = self.get_client_loop()
+        log_loop_coro = self.get_log_loop()
+        # 建立新loop
+        self.build_new_event_loop()
+        # 向新loop中添加任务
+        asyncio.run_coroutine_threadsafe(client_loop_coro, self.loop)
+        asyncio.run_coroutine_threadsafe(log_loop_coro, self.loop)
 
     def stop(self):
-        if self.client_loop:
-            # stop
-            pass
+        if self.loop:
+            try:
+                self.loop.stop()
+                self.loop.close()
+            except:
+                raise Exception('loop 有问题')
+            finally:
+                self.loop = None
+
+    def get_client_loop(self):
+        roomid = self.roomid.get()
+        # todo: 需要用到bilibili_client内容，需要合并
+        # bilibili_client 需要接收两个参数，
+        # roomid: 房间号
+        # queue4log: 通信管道
+        pass
+
+    async def get_log_loop(self):
+        while True:
+            msg = await self.queue4log.get()
+            self.logout(msg)
+
+    def build_new_event_loop(self):
+        if self.loop:
+            # loop存在，必须停止
+            self.stop()
+
+        self.loop = asyncio.new_event_loop()
+
+        t = threading.Thread(target=run_loop(), args=(self.loop,))
+        t.start()
+
+
+def run_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
 
 
 def main():
@@ -67,5 +111,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# todo: gui和主程序有冲突，运行时容易卡死
